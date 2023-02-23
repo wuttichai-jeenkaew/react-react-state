1abfe7b0d452d40acacf5050eb78a9f1637e7a25
knotnapat <napat.wongchr@gmail.com>
2023-02-23 14:23:15 +0700

    add ex

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="greeting-container">Greeting Message</div>
+      <div className="buttons">
+        <button>สวัสดี!</button>
+        <button>Hi!</button>
+        <button>你好!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
